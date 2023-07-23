656af7c260a0ae7e24a191bb5f70b2224fd55ce7
JanbBongJun <pon05167@naver.com>
2023-07-24 00:08:18 +0900

    fix:chatting기능수정

--- socket.js
+++ socket.js
@@ -1,6 +1,6 @@
 const socketIo = require('socket.io');
 const http = require('./app.js');
-const ChattingControllerSocket = require("./controller/chattingController.js")
+const ChattingControllerSocket = require("./controller/chattingController.js");
 const chattingController = new ChattingControllerSocket()
 
 
